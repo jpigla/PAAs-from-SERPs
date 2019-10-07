7ff84712b91164caa995e9532ed33dd750dd996d
jigla <hello@jpigla.de>
2019-10-07 16:35:33 +0200

    first working version; extend gitignore; extend package.json;

--- get_paas.js
+++ get_paas.js
@@ -1,17 +1,15 @@
 const puppeteer = require('puppeteer');
 const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
 
 // ========================== Declare Variables ==========================
 
 let time = new Date().toLocaleString("de-DE",{year: 'numeric', month: '2-digit', day: '2-digit'});
 let timestamp = new Date().getTime();
 
-
-
-// const keyword = argv.kw;
+const keyword = argv.kw;
 const clicks = argv.clicks;
-
-
+const output = argv.output;
 
 // ========================== Declare Functions ==========================
 
@@ -23,110 +21,279 @@ async function asyncForEach(array, callback) {
 
 const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
 
-async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-
-                if(totalHeight >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 150);
-        });
-    });
-};
-
-
 async function get_paas(browser,keyword,clicks){
 
     let url = 'https://www.google.de/search?pws=0&no_sw_cr=1&q='+keyword;
-    let paa;
 
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36');
-
     await page.setViewport({
         width: 1920,
         height: 1080
     });
 
     await page.goto(url, {waitUntil: 'networkidle0'});
-    // await autoScroll(page);
 
-    let paas_length;
-    let paas_length_2;
-    let paas_length_3;
-    let paas_questions;
-    let paas;
-    let error_tmp;
+    if (clicks == 0) {
+        const xpath_expression_question = '//div/div/g-accordion-expander/div[1]/div';
 
-    if (clicks == 1){
-        await page.evaluate(() => {
-            error_tmp = document.getElementsByClassName("g kno-kp mnr-c g-blk")[0].scrollIntoView();
-            paas = document.getElementsByClassName('mWyH1d kno-atc');
-            paas_length = paas.length;
-            async (paas) => {
-                for (i = 0; i < paas.length; i++) {
-                    try {
-                        paas[i].click();
-                        await page.waitFor(1000);
-                        paas[i].click();
-                    } catch (e) {
-                        logMyErrors(e);
-                    }
-                }
-            }
-            paas_questions = document.getElementsByClassName('mWyH1d kno-atc').innerText;
-        });
+        await page.waitForXPath(xpath_expression_question);
+        const questions_without_click = await page.$x(xpath_expression_question);
+        const count_questions = questions_without_click.length;
+
+        const questions = await page.evaluate((...questions_without_click) => {
+
+            return questions_without_click.map(e => e.innerText);
+
+        }, ...questions_without_click);
+
+        return {
+            questions: questions,
+            count: count_questions
+        };
+
+    } else if (clicks == 1) {
+
+        const xpath_expression_question = '//div/div/g-accordion-expander/div[1]/div';
+        await page.waitForXPath(xpath_expression_question);
+
+        await page.evaluate(() => {document.getElementsByClassName("g kno-kp mnr-c g-blk")[0].scrollIntoView();});
+
+        const questions_without_click = await page.$x(xpath_expression_question);
+
+        let counter = 0;
+        if (questions_without_click.length > 0) {
+            for(i in questions_without_click) {
+                await questions_without_click[i].click();
+                await page.waitFor(1000);
+                counter++;
+            };
+        }
+
+        console.log(counter+" questions clicked.");
+
+        const questions_with_1_click = await page.$x(xpath_expression_question);
+        const count_questions = questions_with_1_click.length;
+
+        const questions = await page.evaluate((...questions_with_1_click) => {
+
+            return questions_with_1_click.map(e => e.innerText);
+
+        }, ...questions_with_1_click);
+
+        return {
+            questions: questions,
+            count: count_questions
+        };
+
+    } else if (clicks == 2) {
+
+        const xpath_expression_question = '//div/div/g-accordion-expander/div[1]/div';
+        await page.waitForXPath(xpath_expression_question);
+
+        await page.evaluate(() => {document.getElementsByClassName("g kno-kp mnr-c g-blk")[0].scrollIntoView();});
+
+        const questions_without_click = await page.$x(xpath_expression_question);
+
+        let counter = 0;
+        if (questions_without_click.length > 0) {
+            for(i in questions_without_click) {
+                await questions_without_click[i].click();
+                await page.waitFor(1000);
+                counter++;
+            };
+        }
+
+        console.log(counter+" questions clicked.");
+        
+        await page.waitFor(1000);
+        const questions_with_1_click = await page.$x(xpath_expression_question);
+        await page.waitFor(1000);
+
+        await page.evaluate(() => {document.getElementsByClassName("duf3")[1].scrollIntoView();});
+
+        if (questions_with_1_click.length > counter) {
+            for(counter; counter < questions_with_1_click.length; counter++) {
+                await questions_with_1_click[counter].click();
+                await page.waitFor(1000);
+            };
+        }
+
+        console.log(counter+" questions clicked.");
+        
+        await page.waitFor(1000);
+        const questions_with_2_click = await page.$x(xpath_expression_question);
+        const count_questions = questions_with_2_click.length;
+        await page.waitFor(1000);
+
+        const questions = await page.evaluate((...questions_with_2_click) => {
+
+            return questions_with_2_click.map(e => e.innerText);
+
+        }, ...questions_with_2_click);
+
+        return {
+            questions: questions,
+            count: count_questions
+        };
+
+    } else if (clicks == "max") {
+        // max = 4 rounds if clicks
+
+        const xpath_expression_question = '//div/div/g-accordion-expander/div[1]/div';
+        await page.waitForXPath(xpath_expression_question);
+
+        await page.evaluate(() => {document.getElementsByClassName("g kno-kp mnr-c g-blk")[0].scrollIntoView();});
+
+        const questions_without_click = await page.$x(xpath_expression_question);
+
+        let counter = 0;
+        if (questions_without_click.length > 0) {
+            for(i in questions_without_click) {
+                await questions_without_click[i].click();
+                await page.waitFor(800);
+                counter++;
+            };
+        } 
+        console.log(counter+" questions clicked.");
+
+        await page.waitFor(500);
+        const questions_with_1_click = await page.$x(xpath_expression_question);
+        await page.waitFor(500);
+
+        await page.evaluate(() => {document.getElementsByClassName("duf3")[1].scrollIntoView();});
+
+        if (questions_with_1_click.length > counter) {
+            for(counter; counter < questions_with_1_click.length; counter++) {
+                await questions_with_1_click[counter].click();
+                await page.waitFor(900);
+            };
+        }
+
+        console.log(counter+" questions clicked.");
+        
+        await page.waitFor(500);
+        const questions_with_2_click = await page.$x(xpath_expression_question);
+        await page.waitFor(500);
+
+        await page.evaluate(() => {document.getElementsByClassName("duf3")[1].scrollIntoView();});
+
+        if (questions_with_2_click.length > counter) {
+            for(counter; counter < questions_with_2_click.length; counter++) {
+                await questions_with_2_click[counter].click();
+                await page.waitFor(1000);
+            };
+        }
+
+        console.log(counter+" questions clicked.");
+        
+        await page.waitFor(500);
+        const questions_with_3_click = await page.$x(xpath_expression_question);
+        await page.waitFor(500);
+
+        await page.evaluate(() => {document.getElementsByClassName("duf3")[1].scrollIntoView();});
+
+        if (questions_with_3_click.length > counter) {
+            for(counter; counter < questions_with_3_click.length; counter++) {
+                await questions_with_3_click[counter].click();
+                await page.waitFor(1100);
+            };
+        }
+
+        console.log(counter+" questions clicked.");
+        
+        await page.waitFor(500);
+        const questions_with_4_click = await page.$x(xpath_expression_question);
+        const count_questions = questions_with_4_click.length;
+        await page.waitFor(500);
+
+        const questions = await page.evaluate((...questions_with_4_click) => {
+
+            return questions_with_4_click.map(e => e.innerText);
+
+        }, ...questions_with_4_click);
+
+        return {
+            questions: questions,
+            count: count_questions
+        };
     }
 
+    await page.close();
 
-
-
-    let question;
-    let paas_result = new Array();
-
-    await asyncForEach(paas_questions, async (question) => {
-        paas_result.push(question);
-    });
-
-
-
-
-    return paas_result;
 };
-
-
-
-
 
 
 // ========================== Main Functions ==========================
 
+
 (async function(){
+    
+    let paas_result = new Array();
 
-    await puppeteer.launch({headless: false, args: ['--incognito']}).then(async browser => {
+    await puppeteer.launch({headless: true, args: ['--incognito']}).then(async browser => {
 
-        let lineReader = require('readline').createInterface({
-            input: require('fs').createReadStream('keywords.txt')
-        });
+        if (keyword == "keywords") {
 
-        for await (const keyword of lineReader) {
-            console.log(keyword);
+            let lineReader = require('readline').createInterface({
+                input: require('fs').createReadStream('keywords.txt')
+            });
+
+            for await (const keywords of lineReader) {
+
+                const result = await get_paas(browser,keywords,clicks);
+                
+                console.log('');
+                paas_result.push('== Search term: '+keywords+' ('+result.count+') ==');
+                await asyncForEach(result.questions, async (question) => {
+                    paas_result.push(question);
+                });
+                paas_result.push('');
+                // paas_result.push('== Number of questions found: '+result.count+' ==');
+
+            };
+
+        } else {
 
             const result = await get_paas(browser,keyword,clicks);
-            console.log(result);
             
+            console.log('');
+            paas_result.push('== Search term: '+keyword+' ('+result.count+') ==');
+            await asyncForEach(result.questions, async (question) => {
+                paas_result.push(question);
+            });
+            // paas_result.push('== Number of questions found: '+result.count+' ==');
 
-        };
+        }
 
+        paas_result.forEach(question => {
+            console.log(question);
+        });
+
+        if (output == "csv") {
+
+            let csv_output;
+            let csv_filename = 'paas_'+keyword.replace(" ","+")+'_'+timestamp+'.csv';
+
+            paas_result.pop();
+
+            paas_result.forEach(question => {
+                csv_output += decodeURIComponent(question+'\n');
+            });
+
+            csv_output = csv_output.slice(9);
+
+            fs.writeFile(csv_filename, csv_output, 'utf8', function (err) {
+                if (err) {
+                  console.log('Some error occured while trying to save data to CSV!');
+                } else{
+                  console.log('\nData was saved successfully to CSV! ('+csv_filename+')');
+                }
+              });
+        }
+
+        await browser.close();
     });
 
 })();
